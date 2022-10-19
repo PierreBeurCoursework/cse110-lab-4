9b5c739669427b549c39c00e2eaadcd57e378881
PierreBeur <cbeurtheret@ucsd.edu>
2022-10-19 10:16:13 -0700

    Add question 12

---
+++ expose/javascript/part2-question12.js
@@ -0,0 +1,11 @@
+let student = {
+    name: 'Sarah',
+    major: 'Computer Science',
+    'Grad Year': '2022',
+    greeting: function() { console.log('Hello!'); },
+    'Favorite Teacher': {
+        name: 'Thomas Powell',
+        course: 'CSE 110'
+    },
+    courseLoad: ['CSE 110', 'CSE 134', 'VIS 41']
+};
